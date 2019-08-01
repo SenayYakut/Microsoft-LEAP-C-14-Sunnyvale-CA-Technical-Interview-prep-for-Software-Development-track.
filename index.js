aeccb2058011ff20c33bba1bfc5406ad373f7562
Senay Yakut <senaykt@gmail.com>
2019-08-01 11:53:53 -0700

    The answer of question 5 which is a sum of two

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ function palindrome(str){
 }
 
 palindrome("madam");
+
+
+// Sums Answer;
+
+function twoSum(arr, S){
+    var sums=[];
+    for (var i=0; i<arr.length; i++){
+        for(var j=i+1; j<arr.length; j++){
+            if(arr[i]+arr[j]===S){
+                sums.push([arr[i],arr[j]]);
+            }
+        }
+    }
+    return sums[0];
+
+}
+
+console.log(twoSum([1,2,3,4,5,6],7));
+
