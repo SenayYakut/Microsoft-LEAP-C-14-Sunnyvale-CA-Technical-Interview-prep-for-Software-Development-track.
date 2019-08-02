cdbda89cdfa133eba1870c07d85510a9b1cd63f4
Senay Yakut <senaykt@gmail.com>
2019-08-02 16:59:45 -0700

    some corrections added

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 
 function reversed(str){
     var strReversed={};
-    strReversed+=str.split("").reverse().join();
+    strReversed+=str.split("").reverse().join("");
     return strReversed;
 }
 console.log(reversed("senay"));//yanes
 console.log(reversed("123456"));//654321
-console.log(reversed("I like to cook a banana bread this weekend. Oh yumm!"));//!,m,m,u,y, ,h,O, ,.,d,n,e,k,e,e,w, ,s,i,h,t, ,d,a,e,r,b, ,a,n,a,n,a,b, ,a, ,k,o,o,c, ,o,t, ,e,k,i,l, ,I
+console.log(reversed("I like to cook a banana bread this weekend. Oh yumm!"));//!mmuy hO .dnekeew siht daerb ananab a kooc ot ekil I
