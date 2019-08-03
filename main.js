f824a8c3adf9f5efbe8f087c3af8ea09e162c86f
Senay Yakut <senaykt@gmail.com>
2019-08-02 17:25:40 -0700

    popular algo question 2 answer

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ function reversed(str){
 console.log(reversed("senay"));//yanes
 console.log(reversed("123456"));//654321
 console.log(reversed("I like to cook a banana bread this weekend. Oh yumm!"));//!mmuy hO .dnekeew siht daerb ananab a kooc ot ekil I
+
+//Write a function that checks if the given word palindrome
+
+function palindrome(str){
+    var unWanted=/[\W_]g/;
+    var strLower=str.toLowerCase().replace(unWanted, "");
+    var strReversed=strLower.split("").reverse().join("");
+    return strLower===strReversed;
+}
+console.log(palindrome("senes"));
+console.log(palindrome("nurses run"));
+console.log(palindrome("level-up"));
+console.log(palindrome("333"));
+
