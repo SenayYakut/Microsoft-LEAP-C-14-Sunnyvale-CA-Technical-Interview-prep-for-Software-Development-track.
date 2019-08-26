0727da6b48296b35b8747fad6cd124089c5a9f14
Senay Yakut <senaykt@gmail.com>
2019-08-26 11:11:24 -0700

    implementing stack in javascript

--- stacks.js
+++ stacks.js
@@ -1,7 +1,7 @@
 //Stack is LIFO Which is last in first out, think like a pile of pancakes and when you eat, you eat from top and you eat the first one last
 
-var letters=[];//this is our stack
-var word ="freeCodeCamp";
+var letters=[];//this is my stack
+var word ="senay";
 var rword="";
 
 //put letters of word into stack
@@ -14,6 +14,7 @@ for(let i=0; i<word.length; i++){
     rword+=letters.pop();
 }
 
+//indeed my name is not a palindrome
 if(rword===word){
     console.log(word + " is a palindrome.");
 }else{
@@ -21,5 +22,46 @@ if(rword===word){
 }
 
 //create a stack
+
+var Stack =function(){
+    this.count=0;
+    this.storage={};
+
+    //Adds a value onto the end of the stack
+    this.push=function(value){
+        this.storage[this.count]=value;
+        this.count++;
+    }
+    //Removes and returns the value at the end of the stack
+    this.pop=function(value){
+        if(this.count===0){
+            return indefined;
+        }
+        this.count--;
+        var result=this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+    //returns the size of the stack
+    this.size=function(){
+        return this.count;
+    }
+    //returns the value at the end of stack
+    this.peek=function(){
+        return this.storage[this.count-1];
+    }
+}
+var myStack=new Stack();
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+myStack.push("senay yakut");
+console.log(myStack.size());
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
 
 
