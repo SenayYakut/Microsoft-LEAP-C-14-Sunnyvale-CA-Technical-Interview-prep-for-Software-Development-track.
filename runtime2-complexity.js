317f3df2da1f960fcdeebd9df70f3349b359c468
Senay Yakut <senaykt@gmail.com>
2019-08-29 13:18:20 -0700

    lets think about this approach again

--- runtime2-complexity.js
+++ runtime2-complexity.js
@@ -3,28 +3,60 @@
 //it is 2 loops so it is O(N^2) Exonentially it will grow
 //it is Quadratic
 const findDuplicatesA=(array)=>{
-    const duplicated=[];
+    const duplicates=[];
     for(let i=0; i<array.length; i++){
         for(let j=i+1; j<array.length; j++){
             if(array[i]===array[j]){
-                duplicated.push(array[j]);
+                duplicates.push(array[j]);
             }
         }
     }
-    return duplicated;
+    return duplicates;
 }
 const array=[5,2,4,5,4];
 console.log(findDuplicatesA(array));
 
 
-const findDuplicatesB=(array)=>{
+const findDuplicatesB = (array) => {
+    const seen = new Set();
+    const duplicates = new Set();
+    for (let i = 0; i < array.length; i++) {
+      const value = array[i];
+      if (seen.has(value)) {
+        duplicates.add(value);
+      }
+      seen.add(value);
+    }
+    return duplicates;
+  }
 
-}
-
+  
+// this O(N), Linear, since the array is sorted , we dont have to do two loops , 
 const findDuplicatesC=(array)=>{
-
+    array.sort();
+    const duplicates=[];
+    for(let i=0; i<array.length-1; i++){
+        const item=array[i];
+        const nextItem=array[i+1];
+        if(item===nextItem){
+            duplicates.push(item);
+        }
+    }
+    return duplicates;
 }
+console.log(findDuplicatesC([5,2,4,5,4]));
 
-const findDuplicatesD=(array)=>{
 
-}
+const findDuplicatesD = (array) => {
+    const duplicated = [];
+    const seen = [];S
+    for (let search = 0; search < array.length; search++) {
+      const item = array[search];
+      seen.push(item);
+      if (seen.indexOf(item) !== -1) {
+        duplicated.push(item);
+      }
+    }
+    return duplicated;
+  }
+  console.log(findDuplicatedD([5,2,4,5,4]));
