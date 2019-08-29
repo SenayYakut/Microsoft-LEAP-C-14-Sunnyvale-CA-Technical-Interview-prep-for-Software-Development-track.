a0f4c0a2825d7011ebf56f0db745ed7859871f53
Senay Yakut <senaykt@gmail.com>
2019-08-28 20:10:14 -0700

    more practice

---
+++ runtime2-complexity.js
@@ -0,0 +1,19 @@
+//Compare the following pieces of code that have the same functionality.
+//what is the runtime complexity for each function, think about Big O
+
+const findDuplicatesA=(array)=>{
+    const duplicated=[];
+    
+}
+
+const findDuplicatesB=(array)=>{
+
+}
+
+const findDuplicatesC=(array)=>{
+
+}
+
+const findDuplicatesD=(array)=>{
+
+}
