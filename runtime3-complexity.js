a65525c677bfd6ad4173f80ee5ea782fe5d72e48
Senay Yakut <senaykt@gmail.com>
2019-08-30 12:11:06 -0700

    find the most frequent item of an array

--- runtime3-complexity.js
+++ runtime3-complexity.js
@@ -3,16 +3,18 @@
 //1-Find the integer that occurs most frequently in an array.
 //input: [1,4,5,4,2,2,4];
 //output:4
-
+const array=[1,4,5,4,2,2,4];
 const mostFrequent=(array)=>{
-    let counts={};
-    let most=[];
+    let mf=0;
+    let item;
     for(let i=0; i<array.length; i++){
-        const item = array[i];
-        if(counts[item]===undefined){
-            counts[item]=0;
+        for(let j=i; j<array.length; j++){
+            if(array[i]===array[j])
+                mf++;
         }
-        counts[item]+=1;
-    }
+        item=array[i];
+    } 
+    return item;
 }
-console.log(mostFrequent([1,4,5,4,2,2,4]));
+console.log(mostFrequent(array));
+
