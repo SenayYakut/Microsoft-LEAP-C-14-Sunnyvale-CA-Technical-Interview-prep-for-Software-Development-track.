316fbd92b22969dd49deed52d93257af1bb69b92
Senay Yakut <senaykt@gmail.com>
2019-08-30 13:59:44 -0700

    Linear time complexity

--- runtime3-complexity.js
+++ runtime3-complexity.js
@@ -1,10 +1,11 @@
 //How would you solve these problems, think of an O(n^2), O(n log n), O(n) solutions
 
-//1-Find the integer that occurs most frequently in an array.
+//1-Find the integer that occurs most frequently in an array. it is Linear O(N); PRETTY EFFICENT, WE USE THE HASHMAP.
 const array1=[1,2,2,2,2,3,3,4,4,4,5,12,3,12,3,12,4,12,12];
 const array2=[3,4,5,7,7,7,7];
 const array3=[0];
 const array4=[];
+
 const mostFrequentItem=(array)=>{
     let counts={};
     let compare=0;
@@ -27,5 +28,6 @@ console.log(mostFrequentItem(array1));//12
 console.log(mostFrequentItem(array2));//7
 console.log(mostFrequentItem(array3));//0
 console.log(mostFrequentItem(array4));//undefined
+
 
 
