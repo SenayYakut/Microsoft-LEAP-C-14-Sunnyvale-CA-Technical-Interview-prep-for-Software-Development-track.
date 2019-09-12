5c1420e763779fcef2f635884432d83fc76abcbb
Senay Yakut <senaykt@gmail.com>
2019-09-12 14:54:10 -0700

    more examples

--- objectOrient.js
+++ objectOrient.js
@@ -31,3 +31,25 @@ const drawWinston=function(winston){
     
 
 }
+//object
+var car ={
+    "wheels":4,"engines":1,"seats":5
+};
+//constructor call
+var Car=function(){
+    this.wheels=4;
+    this.engines=1;
+    this.seats=5;
+};
+//constructor definition
+var Car=function(wheels,seats,engines){
+    this.wheels=wheels;
+    this.seats=seats;
+    this.engines=engines;
+};
+var myCar=new Car(8,16,1);
+
+var myCar=new Car();
+myCar.nickname="Meep Meep";
+console.log(myCar);
+
