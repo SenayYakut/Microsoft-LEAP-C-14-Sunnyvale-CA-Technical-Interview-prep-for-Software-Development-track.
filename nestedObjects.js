6545cf8a4e95ea193d40dbc49a2f3f3420b14eb1
Senay Yakut <senaykt@gmail.com>
2019-09-13 12:01:52 -0700

    OOP

---
+++ nestedObjects.js
@@ -0,0 +1,12 @@
+var myFaveMusic=[
+    {
+        "albumTitle":"Into the Trees",
+        "musician":"Zoe Keating",
+        "releaseDate":2010,
+        "songs":{"Forest":{"Length":"2:07", "Instruments":"Cello"},
+                "Intro the Trees":{"Length":"3:15","Instruments":"Cello"}
+        }
+    }
+];
+
+console.log(myFaveMusic[0]["songs"]["Forest"]);
