a80720840c54bd5d1dc64e6861107b7a302d6465
Senay Yakut <senaykt@gmail.com>
2019-09-17 15:59:54 -0700

    hash tables in javascript

--- hashTables.js
+++ hashTables.js
@@ -103,4 +103,35 @@ function HashTable(obj){
     h.clear();
     alert('length after clear: ' + h.length);
    
+    class HashTables{
+        constructor(size=42){
+            this.buckets=new Array(size)
+            this.size=size
+        }
+
+        hash(key){
+            return key.toString().length % this.size;
+        }
+
+        set(key,value){
+            let index=this.hash(key);
+            if(!this.buckets[index]){
+                this.buckets[index]=[];
+            }
+            this.buckets[index].push([key,value])
+            return index
+        }
+
+        get(key){
+            let index =this.hash(key);
+            if(!this.buckets[index])return null
+            for(let bucket of this.buckets[index]){
+                //key
+                if(bucket[0]===key){
+                    return bucket[1]
+                }
+            }
+        }
+    }
+    
     
